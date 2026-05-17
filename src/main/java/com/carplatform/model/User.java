08e4bda0e6a82745d6e53998947f472d3f83ac3b
IT25100694 <it25100694@my.sliit.lk>
2026-05-17 23:17:17 +0530

    User.java

--- src/main/java/com/carplatform/model/User.java
+++ src/main/java/com/carplatform/model/User.java
@@ -1,7 +1,7 @@
 ﻿package com.carplatform.model;
 
 /**
- * IT25100694 – Gunawardhena P.G.M.B.P. | Member 1 – User Management Module
+ * IT25100694 – Gunawardhena P.G.M.B.P | Member 1 – User Management Module
  
  * Abstract base class representing a User in the system.
  *
