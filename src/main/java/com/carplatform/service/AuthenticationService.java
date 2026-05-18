03646ae420d3c95db35960090ad700d7d66efcb8
IT25100694 <it25100694@my.sliit.lk>
2026-05-18 13:50:50 +0530

    AuthenticationService.java

--- src/main/java/com/carplatform/service/AuthenticationService.java
+++ src/main/java/com/carplatform/service/AuthenticationService.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 /**
- * IT25100694 – Gunawardhena P.G.M.B.P. | Component 1 – User Management Module
+ * IT25100694 – Gunawardhena P.G.M.B.P | Component 1 – User Management Module
  
  * Service for user authentication and profile management.
  *
