2b1ca7360433ab8261221983dbde329afe935819
IT25100694 <it25100694@my.sliit.lk>
2026-05-18 13:50:29 +0530

    AuthenticationService.java

--- src/main/java/com/carplatform/service/AuthenticationService.java
+++ src/main/java/com/carplatform/service/AuthenticationService.java
@@ -11,11 +11,12 @@ import java.util.List;
 import java.util.UUID;
 
 /**
+ * IT25100694 – Gunawardhena P.G.M.B.P. | Component 1 – User Management Module
+ 
  * Service for user authentication and profile management.
  *
  * OOP: Encapsulation – all business logic hidden from servlets.
- *      Polymorphism – works with any User subtype.
- * IT25100694 – Gunawardhena P.G.M.B.P. | Component 1 – User Management Module
+ *      Polymorphism – works with any User subtype
  */
 @Service
 public class AuthenticationService {
